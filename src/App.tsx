cbb20e867a8db35f69e51c67a5ad90009f593ca9
Jack Z <jackzezula@tuta.io>
2021-08-18 21:25:04 +1000

    Make octave responsive

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from "react";
-import logo from "./logo.svg";
 import { Octave } from "./Octave";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <Octave />
+    <div className="app">
+      <section className="octave-view">
+        <Octave />
+        <Octave />
+      </section>
     </div>
   );
 }
