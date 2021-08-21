84fe12c08125dd9b988c2dac028833adc642e84f
Jack Z <jackzezula@tuta.io>
2021-08-21 12:46:00 +1000

    Add builder function

--- src/chords/grammar_test.ts
+++ src/chords/grammar_test.ts
@@ -1,5 +1,38 @@
 import { parse } from "./parser.ts";
 
-const examples = ["C#aug", "C", "Cmin", "CMiN#"];
-const results = examples.map(parse);
-console.log(results);
+// Helper functions
+function zip<X, Y>(xs: X[], ys: Y[]) {
+  const result = [];
+  for (let i = 0; i < Math.max(xs!.length, ys!.length); i++) {
+    result.push((xs[i], ys[i]));
+  }
+  return result;
+}
+
+/** Given a list of string lists, return a list of every possible combination of their concatenation (concatenation ordered by the order of the lists).
+ * i.e. given [["a", "b"], ["1", "2"], ["p", "q"]]
+ *       get ["a1p", "a1q", "a2p", "a2q", "b1p", "b1q", "b2p", "b2q"] */
+function build(_xxs: string[][]): string[] {
+  if (_xxs.length <= 0) return [];
+  let result = _xxs[0];
+  const xxs = _xxs!.slice(1);
+  xxs.forEach((ys) => {
+    result = result.flatMap((x) => ys.map((y) => x + y));
+  });
+  return result;
+}
+
+// Constants to exhaustively generate every possible chord
+const TONES = ["C", "D", "E", "F", "G", "A", "B"];
+const TONE_MODIFIERS = ["", "#", "b"];
+const NOTES = build([TONES, TONE_MODIFIERS]);
+const QUALITIES = ["", "maj", "M", "min", "m", "o", "dim", "+", "aug", "#aug"];
+
+// const chords = build(NOTES, QUALITIES);
+const chords = build([NOTES, QUALITIES]);
+console.log(chords);
+
+// const results = cQualities.map(parse);
+// console.log(
+//   results.map((result) => (result.ast == null ? result.errs : result.ast))
+// );
